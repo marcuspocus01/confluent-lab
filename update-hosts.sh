9fb2821e50a14b89d7a71bb26b59c2457d1afd41
marcuspocus1 <mjutras@gmail.com>
2025-09-16 11:58:44 -0400

    update hosts

--- update-hosts.sh
+++ update-hosts.sh
@@ -22,4 +22,26 @@ cat << EOF | sudo tee -a /etc/hosts >/dev/null
 127.0.0.1 postgres
 
 EOF
-echo Done!
+echo Done, isntalling required package!
+
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common 
+
+echo Adding Docker's official GPG key...
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg 
+
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null 
+echo Updating apt sources...
+sudo apt update -y
+echo Installing Docker...
+sudo apt install -y docker-ce docker-ce openjdk-21-jdk
+
+echo download kafka
+wget https://dlcdn.apache.org/kafka/4.1.0/kafka_2.13-4.1.0.tgz 
+tar -xzf kafka_2.13-4.1.0.tgz
+mv kafka_2.13-4.1.0 kafka
+echo moving kafka binaries to /usr/local/bin
+sudo mv kafka /usr/local/bin 
+echo Add to PATH in /etc/environment :/usr/local/bin/kafka/bin 
+sudo sed -i 's|^PATH="\(.*\)"$|PATH="\1:/usr/local/bin/kafka/bin"|' /etc/environment
+source /etc/environment
+echo "Done! Please log out and back in to ensure the PATH is updated."
